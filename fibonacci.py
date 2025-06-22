8df3f90d2e40c215ef5a93100d833a2bf0a45b37
12werfghm <matthew.ludlum@irvingtonschools.org>
2025-06-22 21:02:15 +0000

    reviewed shapes amd fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+x=1
+y=1
+z=0
+a=int(input("Which number?"))
+def fibonacci(a):
+   if a==1:
+      return 1
+   else: 
+      
+
+# x=int(input("Which number?"))
+# def count(x):
+#     if x!=0:
+#         print(x)
+#         count(x-1)
+# count(x)
